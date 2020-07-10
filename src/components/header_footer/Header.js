254d642f017771197f7ce2cf75650b43b464142e
GWilczyk <gwilczyk.github@gmail.com>
2020-07-10 15:34:42 +0200

    Add SideDrawer

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import SideDrawer from './SideDrawer';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import MenuIcon from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
 class Header extends Component {
+  state = {
+    isDrawerOpen: false
+  };
+
+  toggleDrawer = value => {
+    this.setState({ isDrawerOpen: value });
+  };
+
   render() {
     return (
       <AppBar
@@ -20,6 +30,19 @@ class Header extends Component {
             <div className='font_righteous header_logo_venue'>The Venue</div>
             <div className='header_logo_title'>Musical Events</div>
           </div>
+
+          <IconButton
+            aria-label='Menu'
+            color='inherit'
+            onClick={() => this.toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <SideDrawer
+            open={this.state.isDrawerOpen}
+            onClose={value => this.toggleDrawer(value)}
+          />
         </Toolbar>
       </AppBar>
     );
