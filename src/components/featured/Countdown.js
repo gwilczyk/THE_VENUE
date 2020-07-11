e92d55ac0000b3b91d3abeb7d327907f2aa1b05e
GWilczyk <gwilczyk.github@gmail.com>
2020-07-11 13:54:40 +0200

    Finish Countdown

--- src/components/featured/Countdown.js
+++ src/components/featured/Countdown.js
@@ -2,27 +2,48 @@ import React, { Component } from 'react';
 import Slide from 'react-reveal/Slide';
 
 class Countdown extends Component {
-  state = {};
+  state = {
+    deadline: 'Dec, 24, 2020',
+    days: '0',
+    hours: '0',
+    minutes: '0',
+    secondes: '0'
+  };
+
+  getTimeUntil(deadline) {
+    const time = (Date.parse(deadline) - Date.parse(new Date())) / 1000;
+    const secondes = Math.floor(time % 60);
+    const minutes = Math.floor(time / 60) % 60;
+    const hours = Math.floor(time / 3600) % 24;
+    const days = Math.floor(time / 86400);
+    this.setState({ days, hours, minutes, secondes });
+  }
+
+  componentDidMount() {
+    setInterval(() => this.getTimeUntil(this.state.deadline), 1000);
+  }
+
   render() {
+    const { days, hours, minutes, secondes } = this.state;
     return (
       <Slide left delay={1000}>
         <div className='countdown_wrapper'>
           <div className='countdown_top'>event starts in</div>
           <div className='countdown_bottom'>
             <div className='countdown_item'>
-              <div className='countdown_time'>23</div>
+              <div className='countdown_time'>{days}</div>
               <div className='countdown_tag'>days</div>
             </div>
             <div className='countdown_item'>
-              <div className='countdown_time'>2</div>
+              <div className='countdown_time'>{hours}</div>
               <div className='countdown_tag'>hours</div>
             </div>
             <div className='countdown_item'>
-              <div className='countdown_time'>20</div>
+              <div className='countdown_time'>{minutes}</div>
               <div className='countdown_tag'>minutes</div>
             </div>
             <div className='countdown_item'>
-              <div className='countdown_time'>40</div>
+              <div className='countdown_time'>{secondes}</div>
               <div className='countdown_tag'>secondes</div>
             </div>
           </div>
