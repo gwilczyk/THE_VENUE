4f258f5e50ad86abfeb6d87bedcf74429e9751db
GWilczyk <gwilczyk.github@gmail.com>
2020-07-12 14:18:11 +0200

    Trying to push /build directory

---
+++ build/precache-manifest.44bb50ee17014f36164f0a092ca43197.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb6b6ed3e32981f1a11e40026dcb4d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdfb5b137eda1035978a",
+    "url": "/static/css/main.c86d1169.chunk.css"
+  },
+  {
+    "revision": "e79da6a496cb9531c72f",
+    "url": "/static/js/2.ce4230e3.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.ce4230e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdfb5b137eda1035978a",
+    "url": "/static/js/main.84c43e8c.chunk.js"
+  },
+  {
+    "revision": "7b7c417d6d9c04112ab7",
+    "url": "/static/js/runtime-main.24ee2b26.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
